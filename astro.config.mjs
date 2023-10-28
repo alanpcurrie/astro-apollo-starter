b51cfdc794e51a2e9fd031c83c20817ec0b01140
Alan P Currie <alanc909@outlook.com>
2023-10-28 11:20:23 +0100

    build: :rocket: remove netlify middleware config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,7 @@ import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  adapter: netlify(),
   integrations: [tailwind()],
   vite: {
     plugins: [vanillaExtractPlugin()],
