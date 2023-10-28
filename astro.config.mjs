8090f9bac49880e0b111672ce3745f754bd12fcb
Alan P Currie <alanc909@outlook.com>
2023-10-28 11:11:55 +0100

    feat: :rocket: netlify config for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   integrations: [tailwind()],
   vite: {
     plugins: [vanillaExtractPlugin()],
