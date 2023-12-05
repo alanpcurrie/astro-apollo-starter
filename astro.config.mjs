bfebfb83b45cdbf8b8f0bf19afd346474dc6c51b
Alan P Currie <alanc909@outlook.com>
2023-12-05 19:30:29 +0000

    ✨ feat(astro4): astro 4 upgrade
    astro 4 updgrade and sentry spotlight for tracing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,33 +5,40 @@ import react from '@astrojs/react';
 import solid from '@astrojs/solid-js';
 import mdx from "@astrojs/mdx";
 import remarkToc from 'remark-toc';
-import rehypePresetMinify from 'rehype-preset-minify'
+import rehypePresetMinify from 'rehype-preset-minify';
+import spotlightjs from "@spotlightjs/astro";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  experimental: {
+    optimizeHoistedScript: true,
+    contentCollectionCache: true
+  },
   integrations: [
-    tailwind(),
-    mdx(
-      {
-        optimize: true,
-        syntaxHighlight: 'shiki',
-        shikiConfig: { theme: 'dracula' },
-        remarkPlugins: [remarkToc],
-        rehypePlugins: [rehypePresetMinify],
-        remarkRehype: { footnoteLabel: 'Footnotes' },
-        gfm: false,
-      }
-    ),
-    // react(),
-    react({
-      include: ['**/react/*'],
-    }),
-    solid({
-      include: ['**/solid/*'],
-    })
-  ]
-  ,
+    sentry(), 
+    spotlightjs(),
+    tailwind(), 
+    mdx({
+    optimize: true,
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dracula'
+    },
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypePresetMinify],
+    remarkRehype: {
+      footnoteLabel: 'Footnotes'
+    },
+    gfm: false
+  }), react({
+    include: ['**/react/*']
+  }), solid({
+    include: ['**/solid/*']
+  })],
   vite: {
     plugins: [vanillaExtractPlugin()]
   }
